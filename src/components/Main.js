624877884a9fddf420d129f00a36860f61cda83b
jeffocaya <hijeffocaya@gmail.com>
2024-06-13 18:37:46 +0300

    btn

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Main = () => {
   return (
-    <main className="bg-white p-0 rounded shadow-md">
+    <main className="lg:bg-white p-0 rounded h-screen bg-gray-300">
       <div className="container mx-auto p-0">
       <div id="headerSection" className="bg-white mt-2 mb-6 sticky top-0 flex items-center border-b border-gray-300 pb-4">
   <div className='flex items-center'>
@@ -12,7 +12,7 @@ const Main = () => {
       <span className=" text-primary">Generate</span>
     </div>
   </div>
-  <div className=' flex ml-auto items-center space-x-4 lg:hidden'>
+  <div className=' flex ml-auto items-center space-x-4 lg:hidden pr-8'>
     <button className="bg-items-center p-2">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
         <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
@@ -29,7 +29,7 @@ const Main = () => {
         </div>
 
         <div id="BodySection" className=" flex-grow text-center pb-4">
-          <div id="bodycontainer"className='pt-10 pb-60'>
+          <div id="bodycontainer"className='pt-20 bd-40 md:pt-10 md:pb-60'>
             <div id="convert" className="text-3xl md:text-4xl font-semibold">
 Convert DBML to Nest Js
             </div>
@@ -39,16 +39,20 @@ Convert DBML to Nest Js
             </button>
             </div>
             <div id="drop" className="text-gray-600 text-xl">
-              or <span className="text-blue-500 cursor-pointer ">drop DBML file</span>
+              or <span className="text-blue-500 cursor-pointer ">drop DBML file here</span>
             </div>
           </div>
         </div>
 
-        <div id="ButtonSection" className="bg-secondary text-center p-8 sticky bottom-0 mt-4 border-t border-gray-300 ">
-          <button className="bg-white border border-black  text-black py-2 px-20 rounded text-4xl">
-            GENERATE
-          </button>
-        </div>
+        <div id="ButtonSection" className="lg:bg-secondary text-center p-8 sticky bottom-0 mt-4 lg:border-t lg:border-gray-300 flex justify-center">
+  <button className="bg-white border hover:bg-black hover:text-white border-black text-black py-2 px-20 rounded text-4xl flex items-center space-x-2">
+    <span className="font-bold">GENERATE</span>
+    <span>CTRL</span>
+    <img className="w-5" src="enter.svg" alt="Enter Icon" />
+  </button>
+</div>
+
+
      
     </main>
   );
