575f1a2958ea902cf5d09d9e94e42933cf21d6f6
jeffocaya <hijeffocaya@gmail.com>
2024-06-13 11:47:38 +0300

    Created leftNav

---
+++ src/components/LeftNav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const LeftNav = () => {
+  return (
+    <div className="col-span-1 flex items-start">
+      <button className="p-2">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+        </svg>
+      </button>
+    </div>
+  );
+};
+
+export default LeftNav;
