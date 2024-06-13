094f1f3f886fb0aa79f44273b5d33e4818a77e43
jeffocaya <hijeffocaya@gmail.com>
2024-06-13 11:59:39 +0300

    Created the Main

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainContent = () => {
+  return (
+    <main className="col-span-10 bg-white p-6 rounded shadow-md">
+     
+    </main>
+  );
+};
+
+export default MainContent;
