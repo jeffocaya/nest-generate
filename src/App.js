8e740a8de043ffa7eee17abadf26e0808e996201
jeffocaya <hijeffocaya@gmail.com>
2024-06-14 13:01:51 +0300

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import React from 'react';
 import LeftNav from './components/LeftNav';
 import Main from './components/Main';
 import RightNav from './components/RightNav';
-import { ThemeProvider } from './ThemeContext'; // Import ThemeProvider
+
 
 const App = () => {
   return (
-    <ThemeProvider> {/* Ensure ThemeProvider wraps your entire application */}
-      <div className="h-screen">
+    
+  
+    <div className="h-screen">
         <div className="flex-1 grid grid-cols-12 gap-4 max-w-6xl mx-auto p-4">
           <div className="col-span-1">
             <LeftNav />
@@ -21,7 +22,6 @@ const App = () => {
           </div>
         </div>
       </div>
-    </ThemeProvider>
   );
 };
 
