b8ee62ed0d5c727224752fdb309d731a43a5131b
jeffocaya <hijeffocaya@gmail.com>
2024-06-14 13:03:17 +0300

    Created project card

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ProjectCard = () => {
+    
+    return (
+        <div class="flex items-center p-4 shadow-md border border-secondary">
+            <div>
+<img class="w-60 p-2" src="logo.svg"></img>
+            </div>
+            <div class="text-left">
+                <div class='font-bold p-2 '>Project Name</div>
+                <div>From configuration to security, web apps to big data—whatever the infrastructure needs of your application may be, there is a Spring Project to help you build it. Start small and use just what you need—Spring is modular by design.</div>
+            </div>
+</div>
+    )
+};
+
+export default ProjectCard;
