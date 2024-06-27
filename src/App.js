f95908f4a587a4060b5d94fd38e9721166a66aed
jacwida <georgekawume@gmail.com>
2024-06-27 14:54:00 +0300

    Add all changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 // App.js
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LeftNav from "./components/LeftNav";
 import Main from "./components/Main";
 import RightNav from "./components/RightNav";
+import Learn from "./Page/Learn";
+import Project from "./Page/Projects"; // Import the Project component
 
 const App = () => {
   return (
-    <div>
+    <Router>
       <div className="h-screen">
         <div className="flex">
           <div className="border-r border-r-gray-300">
             <LeftNav />
           </div>
           <div className="flex-1">
-            <Main />
+            <Routes>
+              <Route path="/learn" element={<Learn />} />
+              <Route path="/project" element={<Project />} />
+              <Route path="/" element={<Main />} /> {/* Default route */}
+            </Routes>
           </div>
           <div className="border-l border-gray-300">
             <RightNav />
           </div>
         </div>
       </div>
-    </div>
+    </Router>
   );
 };
 
--- src/components/LeftNav.js
+++ src/components/LeftNav.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const LeftNav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation();
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  // Check if current path is /learn or /project
+  if (location.pathname === "/learn" || location.pathname === "/project") {
+    return null; // Don't render LeftNav on Learn and Project pages
+  }
+
   return (
-    <div className="mr-0 items-start hidden md:block w-20 flex-shrink-0">
-      <button className="p-8 w-40 pt-12">
+    <div className="relative">
+      <button className="pt-12 p-4" onClick={toggleMenu}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-6 w-6"
@@ -19,6 +32,77 @@ const LeftNav = () => {
           />
         </svg>
       </button>
+      <div
+        className={`fixed inset-0 w-full h-full bg-primary text-white flex items-center justify-center transform transition-transform duration-500 ${
+          isOpen ? "translate-y-0" : "-translate-y-full"
+        } z-50`}
+      >
+        <div className="absolute top-8 left-4">
+          <button className="p-2 focus:outline-none" onClick={toggleMenu}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className={`h-8 w-8 transition-transform duration-500 transform ${
+                isOpen ? "rotate-0" : "rotate-45"
+              }`}
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+        <div className="text-left">
+          <div className="flex items-center bg-white bg-opacity-80">
+            <img className="w-12 h-12 ml-8 mr-2" src="Logo.svg" alt="Logo" />
+            <div id="siteName" className="p-4 text-3xl md:text-4xl">
+              <span className="font-bold text-black">Nest</span>
+              <span className=" text-primary">Generate</span>
+            </div>
+          </div>
+          <nav className="font-bold text-3xl md:text-4xl pt-8">
+            <ul>
+              <li className="mb-4">
+                <a href="/learn" target="_blank" onClick={toggleMenu}>
+                  NestGenerate Tool
+                </a>
+              </li>
+              <li className="mb-4">
+                <a href="/learn" target="_blank" onClick={toggleMenu}>
+                  Learn About NestGenerate
+                </a>
+              </li>
+              <li>
+                <a href="/project" target="_blank" onClick={toggleMenu}>
+                  Discover All NestGenerate Projects
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="pt-8 text-sm">
+            <div>
+              <a href="#">© 2005-2024 Broadcom. All Rights Reserved.</a>
+            </div>
+            <div>
+              <a href="#">
+                The term "Broadcom" refers to Broadcom Inc. and/or its
+                subsidiaries
+              </a>
+            </div>
+            <div>
+              <a href="#">
+                start.spring.io is powered by Spring Initializr, Cloud Native
+                Buildpacks and Azure Spring Apps
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
